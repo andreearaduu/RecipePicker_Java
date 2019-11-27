9180cc19c39533ceed77565e32ad646784e39d1d
AndreeaMariaRadu <radua6494@gmail.com>
2019-11-27 12:11:27 +0200

    recipe picker project

--- recipe/src/main/java/com/AndreeaRadu/recipe/RecipeApplication.java
+++ recipe/src/main/java/com/AndreeaRadu/recipe/RecipeApplication.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package com.AndreeaRadu.recipe;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class RecipeApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(RecipeApplication.class, args);
 	}
 
 }
--- recipe/src/test/java/com/AndreeaRadu/recipe/RecipeApplicationTests.java
+++ recipe/src/test/java/com/AndreeaRadu/recipe/RecipeApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package com.AndreeaRadu.recipe;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class RecipeApplicationTests {
 
 	@Test
 	void contextLoads() {
