007d755649664bd4f68db9374df2cc44ef1f07fb
AndreeaMariaRadu <radua6494@gmail.com>
2020-03-18 11:49:15 +0200

    in recipeController add methods for getting ingredients and reviews; in userController add methods for getting own,favorite and cooked recipes;

--- recipepicker/src/main/java/com/andreearadu/recipepicker/controller/RecipeController.java
+++ recipepicker/src/main/java/com/andreearadu/recipepicker/controller/RecipeController.java
@@ -7,7 +7,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.andreearadu.recipepicker.dto.IngredientDto;
 import com.andreearadu.recipepicker.dto.RecipeDto;
+import com.andreearadu.recipepicker.dto.ReviewDto;
 import com.andreearadu.recipepicker.service.RecipeService;
 
 @RestController
@@ -16,17 +19,25 @@ public class RecipeController {
 
 	@Autowired
 	RecipeService recipeService;
-	
+
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public Collection<RecipeDto> getAllRecipes() {
-
 		return recipeService.getAllRecipes();
 	}
-	
-	@RequestMapping(value="/recipe", method = RequestMethod.GET)
+
+	@RequestMapping(value = "/recipe", method = RequestMethod.GET)
 	public Collection<RecipeDto> getRecipeByName(@RequestParam("name") String name) {
-		
 		return recipeService.getRecipesByNameLike(name);
 	}
-	
+
+	@RequestMapping(value = "/recipe/ingredients", method = RequestMethod.GET)
+	public Collection<IngredientDto> getIngredientsForRecipe(@RequestParam("id") long id) {
+		return recipeService.getIngredientsForRecipe(id);
+	}
+
+	@RequestMapping(value = "/recipe/reviews", method = RequestMethod.GET)
+	public Collection<ReviewDto> getReviewsForRecipe(@RequestParam("id") long id) {
+		return recipeService.getReviewsForRecipe(id);
+	}
+
 }
