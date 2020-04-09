4cf53c77a991ce70b1d90584a9bc6d6e3d76edc8
AndreeaMariaRadu <radua6494@gmail.com>
2020-04-09 11:58:31 +0300

    adding response entitiy to each method from controllers; updating method of getting recipe filtered; add method of removing recipe from user;

--- recipepicker/src/main/java/com/andreearadu/recipepicker/controller/UserController.java
+++ recipepicker/src/main/java/com/andreearadu/recipepicker/controller/UserController.java
@@ -3,12 +3,16 @@ package com.andreearadu.recipepicker.controller;
 import java.util.Collection;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.andreearadu.recipepicker.dto.RecipeDto;
 import com.andreearadu.recipepicker.dto.UserDto;
@@ -21,25 +25,45 @@ public class UserController {
 	UserService userService;
 
 	@RequestMapping(value = "/", method = RequestMethod.GET, consumes = "application/json", produces = "application/json")
-	public Collection<UserDto> getAllUsers() {
-		return userService.getAll();
+	public ResponseEntity<Collection<UserDto>> getAllUsers() {
+		Collection<UserDto> users = userService.getAll();
+		return ResponseEntity.status(HttpStatus.OK)
+				.body(users);
 	}
 
 	@RequestMapping(value = "/user", method = RequestMethod.GET, consumes = "application/json", produces = "application/json")
-	public UserDto getUserById(@RequestParam("id") long id) {
-		return userService.getUserById(id);
+	public ResponseEntity<UserDto> getUserById(@RequestParam("id") long id) {
+		UserDto userDto = userService.getUserById(id);
+		return ResponseEntity
+				.status(HttpStatus.OK)
+				.body(userDto);
 	}
 
-	@RequestMapping(value = "/{userId}/recipe/", method = RequestMethod.GET, consumes = "application/json", produces = "application/json")
-	public Collection<RecipeDto> getFavoriteRecipesByUser(@PathVariable("userId") long userId,
-			@RequestParam("recipeType") String recipeType) {	
-			return userService.getRecipesForUser(userId, recipeType);
-		
+	@RequestMapping(value = "/{userId}/recipe", method = RequestMethod.GET, consumes = "application/json", produces = "application/json")
+	public ResponseEntity<Collection<RecipeDto>> getRecipesByUser(@PathVariable("userId") long userId,
+			@RequestParam("recipeType") String recipeType) {
+		Collection<RecipeDto> recipes = userService.getRecipesForUser(userId, recipeType);
+		return ResponseEntity
+				.status(HttpStatus.OK)
+				.body(recipes);
 	}
 
 	@RequestMapping(value = "/{userId}/recipe", method = RequestMethod.POST, consumes = "application/json", produces = "application/json")
-	public RecipeDto addRecipeToUser(@PathVariable("userId") long userId, @RequestBody RecipeDto recipeDto) {
-		return userService.addRecipeToUser(recipeDto, userId);
-		
+	public ResponseEntity<RecipeDto> addRecipeToUser(@PathVariable("userId") long userId, @RequestBody RecipeDto recipeDto) {
+		RecipeDto recipe= userService.addRecipeToUser(recipeDto, userId);
+		String location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(recipe.getId())
+                .toUriString();
+		return ResponseEntity
+				.status(HttpStatus.CREATED)
+				.header(HttpHeaders.LOCATION, location)
+				.body(recipe);
+	}
+	@RequestMapping(value = "/{userId}/recipe", method = RequestMethod.DELETE, consumes = "application/json", produces = "application/json")
+	public ResponseEntity<String> deleteRecipeFromUser(@PathVariable("userId") long userId, @RequestParam long recipeId) {
+		 userService.removeRecipeFromUser(recipeId, userId);
+		 return ResponseEntity.accepted().build();
 	}
 }
