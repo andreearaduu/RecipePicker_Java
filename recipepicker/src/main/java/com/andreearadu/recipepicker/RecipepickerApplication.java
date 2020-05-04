e41ce36e31c00568af4d6e6162eec7e5f5e6adb0
AndreeaMariaRadu <radua6494@gmail.com>
2020-05-04 15:43:24 +0300

    no message

--- recipepicker/src/main/java/com/andreearadu/recipepicker/RecipepickerApplication.java
+++ recipepicker/src/main/java/com/andreearadu/recipepicker/RecipepickerApplication.java
@@ -2,10 +2,9 @@ package com.andreearadu.recipepicker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @SpringBootApplication
-public class RecipepickerApplication extends WebSecurityConfigurerAdapter{
+public class RecipepickerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RecipepickerApplication.class, args);
